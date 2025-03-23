6d66c129d246489f354b6920bd0f3193f7ef45c0
serdardevops <serdarselcukdevops@gmail.com>
2025-03-23 17:03:28 +0300

    terraform

---
+++ 01_JenkinsServer/01_Main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+variable "ssh_password" {
+  description = "JenkinsServer için SSH parolası"
+  type        = string
+  sensitive   = true 
+}
+
+resource "null_resource" "jenkins_server" {
+
+  connection {
+    type     = "ssh"
+    host     = "192.168.1.179"   
+    user     = "ubuntu"          
+    password = var.ssh_password  
+    port     = 22                
+  }
