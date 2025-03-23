5a6aeb00a775a9e63ceaa530f609911ee5d66b39
serdardevops <serdarselcukdevops@gmail.com>
2025-03-23 17:19:48 +0300

    Update 02_install.sh

--- 01_JenkinsServer/02_install.sh
+++ 01_JenkinsServer/02_install.sh
@@ -1,16 +1,17 @@
-
 #!/bin/bash
+# install.sh
+
+# Sistem güncelleme
 sudo apt update -y
 
-#java
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+# Java (Temurin 17 JDK)
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update -y
 sudo apt install temurin-17-jdk -y
-/usr/bin/java –version
+/usr/bin/java --version
 
-
-#jenkins
+# Jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
@@ -18,22 +19,23 @@ sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 
-
-#docker
+# Docker
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
-sudo usermod -aG docker jenkins  
-newgrp docker
+sudo usermod -aG docker jenkins
+sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
 
-
-#sonarqube
+# SonarQube
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
-#trivy
+# Trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy -y
+
+# Kurulum tamamlandı mesajı
+echo "JenkinsServer yapılandırması tamamlandı. Erişimler: Jenkins (http://192.168.1.179:8080), SonarQube (http://192.168.1.179:9000)"
